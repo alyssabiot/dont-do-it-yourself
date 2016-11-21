c9594ef7c99ca06a2a0df31062ea89599f93af4c
Alyssa Biot <alyssa.biot@gmail.com>
2016-11-21 15:47:59 +0100

    add cloudinary + navbar v2 after review

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,4 @@ class User < ApplicationRecord
   has_many :skills
   has_many :bookings
   has_many :bookings, through: :skills
-  validates :email, uniqueness: true, format: { with: /\A.+@.+\..{2,3}\z/,
-    message: "not an email" }
-  validates :password, length: { in: 6..20 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121125241) do
+ActiveRecord::Schema.define(version: 20161121144707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,10 @@ ActiveRecord::Schema.define(version: 20161121125241) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "address"
+    t.float    "average_rating"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
