0a09a7e936a927b9e7a1158d52d7436db8cab83c
Alyssa Biot <alyssa.biot@gmail.com>
2016-11-24 18:24:14 +0100

    seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Booking.destroy_all
 Skill.destroy_all
 User.destroy_all
-Booking.destroy_all
 user1 = User.new(email: "alyssa@gmail.com", password: "123456")
 user1.first_name = "Alyssa"
 user1.last_name = "Biot"
