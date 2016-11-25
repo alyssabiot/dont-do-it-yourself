acabec174ff1df965e29214aad875d6603e9ce39
Isabelle Bonnard <isabelle.charon@gmail.com>
2016-11-25 13:11:54 +0100

    database and under tabs

--- db/migrate/20161125100300_create_reviews.rb
+++ db/migrate/20161125100300_create_reviews.rb
@@ -3,7 +3,7 @@ class CreateReviews < ActiveRecord::Migration[5.0]
     create_table :reviews do |t|
       t.string :content
       t.integer :rating
-      t.references :skill, foreign_key: true
+      t.references :booking, foreign_key: true
       t.references :user, foreign_key: true
 
       t.timestamps
